8a70d8ca02d432f6fd660d32e8a2c62ccb87646f
David Martin <DMAMartin@dundee.ac.uk>
2020-10-26 08:27:50 +0000

    initial commit for local version

---
+++ Beginning_Plotting/initLesson.R
@@ -0,0 +1,12 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+suppressMessages(dataset <- read_table2(file.path(.get_course_path(), "R_Programming_Alt", "Beginning_Plotting", "sport.txt")))
+dataset$Sport <- as.factor(dataset$Sport)
---
+++ Reading_Data_into_R/customTests.R
@@ -0,0 +1,23 @@
+# Put custom tests in this file.
+      
+      # Uncommenting the following line of code will disable
+      # auto-detection of new variables and thus prevent swirl from
+      # executing every command twice, which can slow things down.
+      
+      # AUTO_DETECT_NEWVAR <- FALSE
+      
+      # However, this means that you should detect user-created
+      # variables when appropriate. The answer test, creates_new_var()
+      # can be used for for the purpose, but it also re-evaluates the
+      # expression which the user entered, so care must be taken.
+file_exists <- function (x) { file.exists(x)}
+folder_exists <- function(x) {dir.exists(x)}
+
+var_exists <- function(x){
+   val = FALSE
+   try({
+      val <- get(x, globalenv())
+      val=TRUE
+   }, silent=TRUE)
+   val
+}
